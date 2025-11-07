2e8b9412f6dcf4c530daac7e5e57465f8e38c773
Rashadk <rashad.khan1@ontariotechu.net>
2025-11-07 15:47:17 -0500

    had to remove the about project, contact pages because it was messing with the page layout.

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-// Home page
+/* GET home page (single-page) */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
-// About page
-router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About Me' });
-});
-
-// Projects page
-router.get('/projects', function(req, res, next) {
-  res.render('projects', { title: 'Projects' });
-});
-
-// Contact page
-router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact Me' });
-});
+/* Optional: redirect other routes to '/' with anchors */
+router.get('/home', (req, res) => res.redirect('/#home'));
+router.get('/about', (req, res) => res.redirect('/#about'));
+router.get('/projects', (req, res) => res.redirect('/#projects'));
+router.get('/contact', (req, res) => res.redirect('/#contact'));
 
 module.exports = router;
