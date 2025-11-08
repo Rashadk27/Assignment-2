1e482abf889dad5efbac3d01b7bcce4b9c1d7063
Rashadk <rashad.khan1@ontariotechu.net>
2025-11-07 20:15:12 -0500

    changes made for navbar header to include linkden link and github as well as footer changed

--- app.js
+++ app.js
@@ -18,6 +18,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap'));
+app.use('/fortawesome', express.static(__dirname + '/node_modules/@fortawesome/fontawesome-free'));
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/popper.js', express.static(__dirname + '/node_modules/popper.js/dist/popper.min.js'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -35,7 +40,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {title:'Error'});
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page (single-page) */
+/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
-/* Optional: redirect other routes to '/' with anchors */
-router.get('/home', (req, res) => res.redirect('/#home'));
-router.get('/about', (req, res) => res.redirect('/#about'));
-router.get('/projects', (req, res) => res.redirect('/#projects'));
-router.get('/contact', (req, res) => res.redirect('/#contact'));
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'About Me' });
+});
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Projects' });
+});
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Contact Me' });
+});
 
 module.exports = router;
